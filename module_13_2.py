df60c628c47a4e521aa7d2151ddb222820006460
rva174 <rva174@yandex.ru>
2024-11-17 10:47:31 +0500

    init

--- module_13_2.py
+++ module_13_2.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
+
 api = ' '
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
